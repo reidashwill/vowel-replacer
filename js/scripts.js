af0064d6ca88a9e643ebf33680f7502a5a691e75
Reid Ashwill <reidashwill@gmail.com>
2020-03-31 11:29:56 -0700

    finish troublshooting for loops
    co-authored-by: Spencer-Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   var string = "the quick brown fox jumps over the lazy dog";
   var vowels = ["a", "e", "i", "o", "u"];
-  var replace = "regex";
-  var re = new RegExp(replace, vowels);
+
 
   vowels.forEach(function(vowels) {
-    string.replace(re, "-")
-    // string = string.replace(/vowels/g, "-");
+    
+    string = string.replace(/([vowels])/g, "-");
     console.log (string)
     
   })
